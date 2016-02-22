e07e22a38c2d6f508ffcb9a9203d971f2571ab5d
taolichao <xvhandong@sina.com>
2016-02-22 16:10:24 +0800

    taolichao test

---
+++ src/org/java/test/App.java
@@ -0,0 +1,18 @@
+package org.java.test;
+/**
+ * ’‚ «≤‚ ‘
+ *
+ */
+public class App {
+	
+	private String a;
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+	
+}
